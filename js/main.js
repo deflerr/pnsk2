0ee8f66ee35e8bb49c8e78159df4de9e0ace3900
deflerr <vladislavflexer@gmail.com>
2025-11-26 01:06:15 +0700

    final

--- js/main.js
+++ js/main.js
@@ -36,7 +36,9 @@ new Vue({
                     completedAt: null
                 });
                 this.saveData();
-           
+            } else {
+                alert('Карточка должна содержать от 3 до 5 пунктов.');
+            }
         },
 
         updateProgress(card) {
